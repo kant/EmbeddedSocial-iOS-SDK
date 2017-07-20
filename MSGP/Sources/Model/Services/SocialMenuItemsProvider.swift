a8a717ce73dc1ac1814adca213455822f1ff2c62
Igor Popov <igor.popov@akvelon.com>
2017-07-20 19:09:01 +0300

    Better declaration for menu items.

--- MSGP/Sources/Model/Services/SocialMenuItemsProvider.swift
+++ MSGP/Sources/Model/Services/SocialMenuItemsProvider.swift
@@ -32,19 +32,31 @@ class SocialMenuItemsProvider: SideMenuItemsProvider {
         }
     }
     
-    var items = [
-        State.authenticated:[(title: "Home", image: UIImage(asset: Asset.iconLiked)),
-            (title: "Sign In", image: UIImage(asset: Asset.iconLiked)),
-            (title: "Search", image: UIImage(asset: Asset.iconSearch)),
-            (title: "Popular", image: UIImage(asset: Asset.iconPopular)),
-            (title: "My pins", image: UIImage(asset: Asset.iconPins)),
-            (title: "Activity Feed", image: UIImage(asset: Asset.iconActivity)),
-            (title: "Settings", image: UIImage(asset: Asset.iconSettings)),
-            (title: "Log out", image: UIImage(asset: Asset.iconLogout))],
-        State.unauthenticated:[(title: "Popular", image: UIImage(asset: Asset.iconPopular)),
-            (title: "Sign In", image: UIImage(asset: Asset.iconLiked)),
-        ]
-    ]
+    typealias ModuleBuilder = (_ coordinator: CrossModuleCoordinator) -> UIViewController
+    
+    static var builderForSignIn: ModuleBuilder = { coordinator in
+        
+        let module = LoginConfigurator()
+        module.configure(moduleOutput: coordinator)
+        return module.viewController
+    }
+    
+    static var builderForDummy: ModuleBuilder = { coordinator in
+        return UIViewController()
+    }
+    
+    var items = [State.authenticated: [
+        (title: "Home", image: UIImage(asset: Asset.iconLiked), builder: builderForSignIn),
+         (title: "Search", image: UIImage(asset: Asset.iconSearch), builder: builderForDummy),
+         (title: "Popular", image: UIImage(asset: Asset.iconPopular), builder: builderForDummy),
+         (title: "My pins", image: UIImage(asset: Asset.iconPins), builder: builderForDummy),
+         (title: "Activity Feed", image: UIImage(asset: Asset.iconActivity), builder: builderForDummy),
+         (title: "Settings", image: UIImage(asset: Asset.iconSettings), builder: builderForDummy),
+         (title: "Log out", image: UIImage(asset: Asset.iconLogout), builder: builderForDummy)
+        ], State.unauthenticated: [
+        (title: "Search", image: UIImage(asset: Asset.iconSearch), builder: builderForSignIn),
+        (title: "Popular", image: UIImage(asset: Asset.iconLiked), builder: builderForSignIn)
+    ]]
     
     func numberOfItems() -> Int {
         return items[state]!.count
@@ -58,27 +70,16 @@ class SocialMenuItemsProvider: SideMenuItemsProvider {
         return items[state]![index].title
     }
     
+    @objc func test() -> UIViewController {
+        return UIViewController()
+    }
+    
     func destination(forItem index: Int) -> UIViewController {
         
-        guard let route = Route(rawValue: index) else {
+        guard let builder = items[.authenticated]?[index].builder else {
             return UIViewController()
         }
         
-        var destinationVC: UIViewController? = nil
-        
-        switch route {
-        case .home:
-            
-            destinationVC = UIViewController()
-            destinationVC?.view.backgroundColor = UIColor.purple
-            
-        case .signin:
-            
-            let loginModule = LoginConfigurator()
-            loginModule.configure(moduleOutput: coordinator)
-            destinationVC = loginModule.viewController
-        }
-        
-        return destinationVC!
+        return builder(self.coordinator)
     }
 }
